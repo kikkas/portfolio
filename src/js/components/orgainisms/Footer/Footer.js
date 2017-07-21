267f055248f16dbd3465d2de399730abf71f05c6
serhiipalash <serhii.palash@gmail.com>
2017-07-21 14:08:53 +0300

    add ‘light’ modifier to Footer

--- src/js/components/orgainisms/Footer/Footer.js
+++ src/js/components/orgainisms/Footer/Footer.js
@@ -5,14 +5,25 @@
  */
 
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import './style.css'
 
 
 export default class Footer extends React.Component {
+  static propTypes = {
+    light: PropTypes.bool,
+  }
+
   render() {
+    const { light } = this.props
+
     return (
-      <div className="footer">
+      <div
+        className={`footer ${
+          light ? 'footer--light' : ''
+        }`}
+      >
         <div className="footer__content">
           <div className="footer__col">
             Martin Kikkas
