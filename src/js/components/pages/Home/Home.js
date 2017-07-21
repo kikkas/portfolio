fdb043200af19c6c769cbff6b250a6da8f248fa0
serhiipalash <serhii.palash@gmail.com>
2017-07-21 09:10:33 +0300

    add TopPanel, Projects, ServiceList and Articles to Home page

--- src/js/components/pages/Home/Home.js
+++ src/js/components/pages/Home/Home.js
@@ -7,10 +7,15 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import Helmet from "react-helmet"
+import PropTypes from 'prop-types'
 
 import Navbar from 'components/orgainisms/Navbar'
 import Footer from 'components/orgainisms/Footer'
 import Content from 'components/orgainisms/Content'
+import TopPanel from 'components/orgainisms/TopPanel'
+import Projects from 'components/orgainisms/Projects'
+import ServiceList from 'components/orgainisms/ServiceList'
+import Articles from 'components/orgainisms/Articles'
 
 import './style.css'
 
@@ -18,11 +23,17 @@ import './style.css'
 @connect(
   ({
     app: { locale },
+    projects,
   }) => ({
     locale,
+    projects,
   })
 )
 export default class Home extends Component {
+  static propTypes = {
+    projects: PropTypes.object,
+  }
+
   render() {
     return (
       <div className="home-page">
@@ -31,12 +42,10 @@ export default class Home extends Component {
         <Navbar />
 
         <Content>
-          <div>
-            Content
-          </div>
-          <div>
-            More content
-          </div>
+          <TopPanel />
+          <Projects projects={this.props.projects} />
+          <ServiceList />
+          <Articles />
         </Content>
 
         <Footer />
